8f97e80c82f17727dd89890fb14665af7f1a4b36
Magnus Austrheim <Magnus.Austrheim@nav.no>
2020-10-28 12:48:09 +0100

    Use GlobalScope

--- src/test/kotlin/com/github/magnuvau/launchdemo/LaunchDemoApplicationTests.kt
+++ src/test/kotlin/com/github/magnuvau/launchdemo/LaunchDemoApplicationTests.kt
@@ -1,7 +1,6 @@
 package com.github.magnuvau.launchdemo
 
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -26,12 +25,12 @@ class LaunchDemoApplicationTests {
 	fun `Asynchronous consumers run in parallell`() {
 		var timer = System.currentTimeMillis()
 
-		runBlocking {
-			launch {
-				ConsumerController().asynchronous(1000)
-				ConsumerController().asynchronous(2000)
-				ConsumerController().asynchronous(3000)
-			}
+		val job1 = GlobalScope.launch {ConsumerController().asynchronous(1000) }
+		val job2 = GlobalScope.launch {ConsumerController().asynchronous(2000) }
+		val job3 = GlobalScope.launch {ConsumerController().asynchronous(3000) }
+
+		while (job1.isActive || job2.isActive || job3.isActive) {
+			Thread.sleep(20)
 		}
 
 		timer = System.currentTimeMillis() - timer
@@ -40,5 +39,4 @@ class LaunchDemoApplicationTests {
 
 		assertTrue(timer in 3000..4000)
 	}
-
 }
